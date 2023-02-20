b7f293a24e643afadfe7ac2cff87d8790c4bc90b
Yerassyl Bekberov <yerassyl_bekberov@epam.com>
2023-02-20 14:00:32 +0500

    feat: setting authentication flow

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
-import "./navigation.styles.scss";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
 
 const Navigation = () => {
  return (
@@ -13,6 +13,9 @@ const Navigation = () => {
     <div className="nav-links-container">
      <Link className="nav-link" to="/shop">
       SHOP
+     </Link>
+     <Link className="nav-link" to="/sign-in">
+      SIGN IN
      </Link>
     </div>
    </div>
