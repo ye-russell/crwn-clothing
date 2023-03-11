83a1f55b8e07a3168842105bd9de32847405cc47
ye-russell <bekberata@gmail.com>
2023-03-11 11:45:39 +0600

    feat: add card-dropdown

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,12 +1,20 @@
-import { Fragment, useContext } from 'react';
+import { Fragment, useContext, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import CartIcon from '../cart-icon/cart-icon.component';
 import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const [ isCartDropdownOpened, setCartDropdownState ] = useState(false);
+
+  const toggleDropdown = () => {
+    console.log(isCartDropdownOpened);
+    setCartDropdownState(!isCartDropdownOpened);
+  };
 
   return (
     <Fragment>
@@ -27,7 +35,11 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <div onClick={toggleDropdown}>
+            <CartIcon />
+          </div>
         </div>
+        {isCartDropdownOpened && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -6,7 +6,7 @@ import './shop.styles.scss';
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   return (
-    <div className='products-container'>
+    <div className="products-container">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
